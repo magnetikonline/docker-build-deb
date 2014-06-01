a9dc818eed2ea2a8bb065dd67a11560218a95f42
Peter Mescalchin <peter@magnetikonline.com>
2014-06-01 21:09:03 +1000

    Upped PHP to 5.5.13

--- phpfpm/extractdeb.sh
+++ phpfpm/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 containerID=`sudo docker run -d magnetikonline/buildphpfpm`
-sudo docker cp $containerID:/root/build/php-5.5.12/php_5.5.12-1_amd64.deb .
+sudo docker cp $containerID:/root/build/php-5.5.13/php_5.5.13-1_amd64.deb .
 sleep 1
 sudo docker rm $containerID
