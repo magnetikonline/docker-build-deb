430f2afe949b010556dc9fc5da13f8a1676ea675
Peter Mescalchin <peter@magnetikonline.com>
2014-08-15 16:11:54 +1000

    Upped Nginx to 1.6.1

--- nginx/extractdeb.sh
+++ nginx/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 containerID=`sudo docker run -d magnetikonline/buildnginx`
-sudo docker cp $containerID:/root/build/nginx-1.6.0/nginx_1.6.0-1_amd64.deb .
+sudo docker cp $containerID:/root/build/nginx-1.6.1/nginx_1.6.1-1_amd64.deb .
 sleep 1
 sudo docker rm $containerID
