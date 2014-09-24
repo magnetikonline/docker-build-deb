20d6193969ae952a250f6f25572faa13ad1c7745
Mark Kimsal <metrofindings@gmail.com>
2014-09-24 12:43:59 -0400

    Added build flags to enable bz2, zlib, ctype, ssl
    , ... phar, pdo mysql, pdo dblib, filter

--- phpfpm/resource/configure.sh
+++ phpfpm/resource/configure.sh
@@ -2,6 +2,7 @@
 
 ./configure \
 	--with-config-file-path=/etc/php5 \
+	--with-config-file-scan-dir=/etc/php5/conf.d \
 	--disable-all \
 	--disable-cgi \
 	--enable-bcmath \
@@ -13,6 +14,11 @@
 	--enable-opcache \
 	--enable-session \
 	--enable-xml \
+	--enable-ctype \
+	--with-bz2 \
+	--with-zlib \
+	--with-mssql \
+	--enable-phar \
 	--with-curl \
 	--with-gd \
 	--with-jpeg-dir=/usr \
@@ -20,4 +26,10 @@
 	--with-mysqli \
 	--with-png-dir=/usr \
 	--with-sqlite3 \
+	--with-gnu-ld \
+	--enable-pdo \
+	--with-pdo-dblib \
+	--with-pdo-mysql \
+	--enable-filter \
+	--with-openssl \
 	--without-pear
