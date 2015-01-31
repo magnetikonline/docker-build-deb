54dcb788d591832fd8b45d3eabfd5128a0434109
Peter Mescalchin <peter@magnetikonline.com>
2015-01-31 20:08:13 +1100

    Updated PHP to 5.6.5, base image is now Ubuntu 14.04.1

--- phpfpm/extractdeb.sh
+++ phpfpm/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 containerID=`sudo docker run -d magnetikonline/buildphpfpm`
-sudo docker cp $containerID:/root/build/php-5.6.3/php_5.6.3-1_amd64.deb .
+sudo docker cp $containerID:/root/build/php-5.6.5/php_5.6.5-1_amd64.deb .
 sleep 1
 sudo docker rm $containerID
