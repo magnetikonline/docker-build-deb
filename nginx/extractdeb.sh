598ec2e9b841ee12d842c8746a5e763e28e9a53f
Peter Mescalchin <peter@magnetikonline.com>
2015-06-07 13:12:45 +1000

    Upped to Nginx 1.8.0

--- nginx/extractdeb.sh
+++ nginx/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-containerID=`sudo docker run -d magnetikonline/buildnginx`
-sudo docker cp $containerID:/root/build/nginx-1.6.2/nginx_1.6.2-1_amd64.deb .
+containerID=$(sudo docker run -d magnetikonline/buildnginx)
+sudo docker cp $containerID:/root/build/nginx-1.8.0/nginx_1.8.0-1_amd64.deb .
 sleep 1
 sudo docker rm $containerID
