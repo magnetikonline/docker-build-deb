a3724dfab16703391b762fa8d1d9b831091480c3
Peter Mescalchin <peter@magnetikonline.com>
2015-06-07 13:53:19 +1000

    Upped PHP to 5.6.9

--- phpfpm/extractdeb.sh
+++ phpfpm/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-containerID=`sudo docker run -d magnetikonline/buildphpfpm`
-sudo docker cp $containerID:/root/build/php-5.6.7/php_5.6.7-1_amd64.deb .
+containerID=$(sudo docker run -d magnetikonline/buildphpfpm)
+sudo docker cp $containerID:/root/build/php-5.6.9/php_5.6.9-1_amd64.deb .
 sleep 1
 sudo docker rm $containerID
