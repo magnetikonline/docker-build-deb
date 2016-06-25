e6d5f77c239f18470b7e37b65b56bf8a80d7746e
Peter Mescalchin <peter@magnetikonline.com>
2016-06-25 17:44:39 +1000

    Upped PHP to 7.0.8

--- phpfpm/extractdeb.sh
+++ phpfpm/extractdeb.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
 containerID=$(docker run --detach magnetikonline/buildphpfpm)
-docker cp "$containerID:/root/build/php-7.0.6/php_7.0.6-1_amd64.deb" .
+docker cp "$containerID:/root/build/php-7.0.8/php_7.0.8-1_amd64.deb" .
 sleep 1
 docker rm $containerID
