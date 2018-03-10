44ae92dc2228a25ea512a6bc9761c07e932cc6be
Peter Mescalchin <peter@magnetikonline.com>
2018-03-10 13:57:16 +1100

    Upped PHP to 7.2.3

--- phpfpm/extractdeb.sh
+++ phpfpm/extractdeb.sh
@@ -4,6 +4,6 @@ DIRNAME=$(dirname "$0")
 
 
 containerID=$(docker run --detach magnetikonline/buildphpfpm)
-docker cp "$containerID:/root/build/php-7.2.0/php_7.2.0-1_amd64.deb" "$DIRNAME"
+docker cp "$containerID:/root/build/php-7.2.3/php_7.2.3-1_amd64.deb" "$DIRNAME"
 sleep 1
 docker rm $containerID
